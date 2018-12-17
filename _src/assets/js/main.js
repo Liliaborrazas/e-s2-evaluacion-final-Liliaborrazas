6f85d1b6ddbb1b7894db5860d0f8340788073e8b
lilia <liliaborrazas14@gmail.com>
2018-12-17 10:14:08 +0100

    show list

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('>> Ready :)');
+//Recojo los campos con los que voy a trabajar
+const api =`http://api.tvmaze.com/search/shows?q=`;
+const input = document.querySelector('#search');
+const films = document.querySelector('.films');
+const btn = document.querySelector('.btn');
+const title = document.querySelector('.title');
+
+function search(){
+  const userSearch = input.value;
+  console.log(userSearch);
+  fetch(api+userSearch)
+    .then(response=> response.json())
+    .then(data=>{
+
+      let items='';
+      for(let i=0; i<data.length; i++){
+        const nameFilms= data[i].show.name;
+        const imagesFilms= data[i].show.image;
+
+        items+=`<li class="films__list">${nameSeries}</li>
+        <img class="films__list">${data[i].show.image}</img>`
+
+      }
+    films.innerHTML = items;
+    });
+}
+btn.addEventListener('click', search);
+
