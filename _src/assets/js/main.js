22b98486f54559e29eb5db433fe5bbde26dab58f
lilia <liliaborrazas14@gmail.com>
2018-12-18 08:46:54 +0100

    change name for id localstorage

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,7 +4,6 @@ const api =`http://api.tvmaze.com/search/shows?q=`;
 const input = document.querySelector('#search');
 const films = document.querySelector('.films');
 const btn = document.querySelector('.btn');
-//const title = document.querySelector('.title');
 
 const search = ()=>{
   const userSearch = input.value;
@@ -16,20 +15,23 @@ const search = ()=>{
         console.log(data[i]);
         const nameFilms= data[i].show.name;
         const imagesFilms= data[i].show.image;
+        const idFilms= data[i].show.id;
         if(imagesFilms === null ){
           items+=`<li class="films__list">
           <div class="container__films">
+          <div class="films__list-id">${idFilms}</div>
           <img src="https://via.placeholder.com/210x295/cccccc/666666/?text=TV" class="films__list-img">
           <h2 class="films__list-name">${nameFilms}</h2>
           </div>
-      </li>`
+      </li>`;
         }else{
           items+=`<li class="films__list">
           <div class="container__films">
+          <div class="films__list-id">${idFilms}</div>
           <img src="${imagesFilms.original}" class="films__list-img">
           <h2 class="films__list-name">${nameFilms}</h2>
           </div>
-        </li>`
+        </li>`;
         }
       }
       films.innerHTML = items;
@@ -46,9 +48,9 @@ let arrayFilms =[];
 
 const favorite = (e)=>{
   const author = e.currentTarget;
-  const nameFilm = author.querySelector('.films__list-name');
-  console.log(nameFilm);
-  const nameTitle = nameFilm.innerHTML;
+  const idFilm = author.querySelector('.films__list-id');
+  console.log(idFilm);
+  const nameTitle = idFilm.innerHTML;
   console.log(nameTitle);
   author.classList.toggle('marFavorit');
   if(arrayFilms.includes(nameTitle)){
