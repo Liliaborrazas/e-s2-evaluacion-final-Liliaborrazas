84c5bd069636b9d32055eda75b7ec778485502ca
lilia <liliaborrazas14@gmail.com>
2018-12-18 12:40:28 +0100

    done commit

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,6 +4,8 @@ const api =`http://api.tvmaze.com/search/shows?q=`;
 const input = document.querySelector('#search');
 const films = document.querySelector('.films');
 const btn = document.querySelector('.btn');
+let arrayFilms =[];
+
 
 const search = ()=>{
   const userSearch = input.value;
@@ -12,61 +14,54 @@ const search = ()=>{
     .then(data=>{
       let items='';
       for(let i=0; i<data.length; i++){
-        console.log(data[i]);
         const nameFilms= data[i].show.name;
         const imagesFilms= data[i].show.image;
         const idFilms= data[i].show.id;
         if(imagesFilms === null ){
           items+=`<li class="films__list">
-          <div class="container__films">
-          <div class="films__list-id">${idFilms}</div>
-          <img src="https://via.placeholder.com/210x295/cccccc/666666/?text=TV" class="films__list-img">
-          <h2 class="films__list-name">${nameFilms}</h2>
-          </div>
-      </li>`;
+        <div class="container__films">
+        <div class="films__list-id">${idFilms}</div>
+        <img src="https://via.placeholder.com/210x295/cccccc/666666/?text=TV" class="films__list-img">
+        <h2 class="films__list-name">${nameFilms}</h2>
+        </div>
+        </li>`;
         }else{
           items+=`<li class="films__list">
-          <div class="container__films">
-          <div class="films__list-id">${idFilms}</div>
-          <img src="${imagesFilms.original}" class="films__list-img">
-          <h2 class="films__list-name">${nameFilms}</h2>
-          </div>
+        <div class="container__films">
+        <div class="films__list-id">${idFilms}</div>
+        <img src="${imagesFilms.original}" class="films__list-img">
+        <h2 class="films__list-name">${nameFilms}</h2>
+        </div>
         </li>`;
         }
-      }
-      films.innerHTML = items;
 
-      const list= document.querySelectorAll('.films__list');
-      for(let i=0; i<list.length; i++){
-        list[i].addEventListener('click', favorite);
-      }
 
+
+        films.innerHTML = items;
+
+        const list= document.querySelectorAll('.films__list');
+        for(let i=0; i<list.length; i++){
+          list[i].addEventListener('click', favorite);
+        }
+      }
     });
-
 };
-let arrayFilms =[];
 
-const favorite = (e)=>{
+
+
+function favorite(e){
   const author = e.currentTarget;
   const idFilm = author.querySelector('.films__list-id');
-  console.log(idFilm);
   const nameTitle = idFilm.innerHTML;
-  console.log(nameTitle);
   author.classList.toggle('marFavorit');
   if(arrayFilms.includes(nameTitle)){
-    //console.log(`${nameTitle} is in arrayFilms`);
     const indexFav = arrayFilms.indexOf(nameTitle);
-    //console.log(indexFav);
-    //console.log(arrayFilms);
     arrayFilms.splice(indexFav, 1);
-    //console.log(arrayFilms);
   }else{
-    //console.log(`${nameTitle} is not in arrayFilms`);
     arrayFilms.push(nameTitle);
-    //console.log(arrayFilms);
   }
   localStorage.setItem('favorit', arrayFilms);
-};
+}
 
 
 
